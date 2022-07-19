1cbbb164b73bf8217b3f39161fb533f491d3b5ad
RimaPaikrao <rimapaikrao72@gmail.com>
2022-07-19 22:30:54 +0530

    First Commit

---
+++ Calculate.java
@@ -0,0 +1,27 @@
+class Calculate
+{
+public static void main(String args[])
+{
+  int a,b,sum,sub,mul,mod;
+  double div;
+  a=5;
+  b=6;
+  sum=a+b;
+  sub=a-b;
+  mul=a*b;
+  div=a/b;
+  mod=a%b;
+ 
+  System.out.println("The sum of two numbers is " + sum );
+ 
+  System.out.println("The subtraction of two numbers is " + sub );
+
+  System.out.println("The Multiplication of two numbers is " + mul );
+
+  System.out.println("The Division of two numbers is " + div );
+
+  System.out.println("The Modulus of two numbers is " + mod );
+
+}
+
+}
