c7eb116d66a0f286d9a4c76baf30e663b754c10f
RimaPaikrao <rimapaikrao72@gmail.com>
2022-07-20 00:57:23 +0530

    added features

---
+++ AdvanceCalculate.java
@@ -0,0 +1,13 @@
+public class AdvanceCalculate {
+    public static void main(String args[])
+{
+
+    int a,b;
+    a=40;
+    b=60;
+    int mult;
+    mult=a*b;
+    System.out.println(mult);
+    
+}
+}
